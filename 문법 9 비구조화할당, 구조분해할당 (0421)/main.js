189b9160ff0d2befd58e2040b6b62d44b2fe7429
hyehyeon0215 <qwert0880@naver.com>
2023-04-24 10:21:52 +0900

    비구조화할당 설명 추가

--- 문법 9 비구조화할당, 구조분해할당 (0421)/main.js
+++ 문법 9 비구조화할당, 구조분해할당 (0421)/main.js
@@ -77,11 +77,63 @@ const studentInfo = [
 
     const {size : {width, height}, color : {fontColor, bgColor}} = styles;
 
-
     // 비구조화할당을 사용하는 궁극적인 목적은, 코드의 가독성을 높이고, 디폴트 파라미터를 설정하기 위한 방법으로 사용함
 
 
-    const MyInfo = (obj) => {`제 이름은 ${obj.name}이고 ${obj.age}살이며 직업은 ${obj.job}입니다`};
+    // const MyInfo = (obj) => {`제 이름은 ${obj.name}이고 ${obj.age}살이며 직업은 ${obj.job}입니다`};
+    // console.log(David({name: "홍길동", age: 20, job: '개발자'}));
+    // 오류 코드
 
-    console.log(David({name: "홍길동", age: 20, job: '개발자'}));
 
+
+
+
+
+
+/*     const myInfo = (name, age, job, hobby) => `제 이름은 ${name}이고 나이는 ${age}이며 직업은 ${job}입니다 그리고 ${hobby}을 좋아합니다`;
+    // {} 안에 ``(백틱)을 사용하게 되면 {} 때문에 백틱이 무시가 됨
+    // ``을 {}로 묶으면 인수 안에 인수를 넣는 형태가 되기 때문에 undefind가 됨
+
+    console.log(myInfo('홍길동', 20, '개발자', '게임'));
+
+    // 실무에서 가장 중요한 것은 협업이기 때문에, 걸림돌이 되는 것은 지양해야 함 */
+    // 안 씀
+
+
+
+
+
+/* const myInfo = (obj) => `제 이름은 ${obj.name}이고 나이는 ${obj.age}이며 직업은 ${obj.job}입니다 그리고 ${obj.hobby}을 좋아합니다`;
+
+// obj란 함수를 호출하는 곳에서 인수를 객체로 넣어주는데, 그 넣는 인수 객체를 받아주는 매개변수라고 할 수 있음
+// obj는 변수이고 변수 안 각각의 키 값으로 들어가 있는 값들을 꺼내어 각 해당하는 부분에
+// name, age, job 등이 들어가야 하므로 각각의 값에 obj.name 등으로 객체 안의 값을 불러와야 합니다
+
+console.log(myInfo({
+    name: '홍길동',
+    age: 20,
+    job: '개발자',
+    hobby: '게임'
+}));
+
+// (매개변수를 객체로 바인딩 한 방식)
+단점: 코드의 가독성이 떨어지고, 디폴트 파라미터를 넣을 수 없음
+안 씀 */
+
+
+
+
+
+const myInfo = ({name, age, job='무직', hobby}) => `제 이름은 ${name}이고 나이는 ${age}이며 직업은 ${job}입니다 그리고 ${hobby}을 좋아합니다`;
+
+console.log(myInfo({
+    name: '홍길동',
+    age: 20,
+    job: '개발자',
+    hobby: '게임'
+}));
+
+// 비구조화할당
+// 코드의 가독성을 높이기 위하여 obj를 객체를 구조분해할당으로 매개변수 자리에 넣어줌으로 바로 해당 키 값을 알 수 있고,
+// 값을 넣는 구간에도 obj.name처럼 불필요한 변수를 사용하지 않고 직관적인 값을 넣어 줄 수 있음
+// 또한 구조분해할당으로 넣는 매개변수 자리에 디폴트 파라미터를 넣어 줄 수도 있음
