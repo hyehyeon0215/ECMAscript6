e2f5040f8375fc83d6e7264936e79c5a74c138d0
hyehyeon0215 <qwert0880@naver.com>
2023-04-24 11:06:56 +0900

    set을 이용한 병합 내용 추가

--- 문법 11 배열과 객체의 합치기와 병합 (0421)/main.js
+++ 문법 11 배열과 객체의 합치기와 병합 (0421)/main.js
@@ -62,4 +62,47 @@ const dup2 = dup.reduce((arr, cur)=>arr.includes(cur) ? arr : [...arr, cur],[]);
 // arr = [1,2,3]
 
 
-console.log(dup2);
+// console.log(dup2);
+
+
+
+
+/* set 객체는 중복되지 않는 유일한 값들의 집합
+set 객체는 동일한 값을 중복해서 포함할 수 없으며, 요소 순서는 의미가 없음
+인덱스 요소를 접근할 수 없고, 따라서 수학적인 집합을 구현하기 위한 자료구조 */
+
+const set = new Set();
+console.log(set);
+
+
+const set1 =new Set([1,2,3,3,3,4,4,4,5]);
+console.log(set1);
+
+// 배열로 값을 중복해서 제공하면 중복된 값을 제거한 고유 값으로 정렬함과 동시에 객체로 반환
+// set은 객체로 만드는 메서드
+
+
+const set2 = new Set("Hello");
+console.log(set2);
+
+// 문자열도 받아서 객체로 만들음 (중복 값 제거)
+
+
+// const set3 = [...new set([1,2,2,3,4,4])];
+// console.log(set3);
+
+// set은 객체로만 반환이 되지만 값을 배열로 담아야 할 때 사용하는 방법은 전개연산자
+// 중복을 제거할 해당 배열을 set으로 반환한 뒤 전개연산자를 이용하여 값을 풀어준 뒤 다시 []으로 감싸 배열로 할당
+
+
+
+// 전개연산자를 통한 객체의 통합
+
+const obj1 = {name: 'David', age: 20};
+const obj2 = {name: 'Andy', address: 'seoul'};
+const com = {...obj1, ...obj2};
+console.log(com);
+
+// 객체의 키 값은 유니크 한 값으로 중복을 허용하지 않음
+// 따라서 객체가 병합될 때 뒤에 병합되는 값으로 앞의 값이 덮어 써지면서 병합이 됨
+// 즉 키 값의 고유성이 지켜짐
